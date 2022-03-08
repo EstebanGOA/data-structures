2cca4d9ecc14638fdf0c1e733883b578a6806755
Esteban Ariel Gómez Agüero <estebanariel.gomez@students.salle.url.edu>
2022-03-08 09:52:10 +0100

    Menú para la segunda fase

--- src/entities/Menu.java
+++ src/entities/Menu.java
@@ -29,6 +29,9 @@ public class Menu {
                 case 1 -> {
                     executeFirstOption();
                 }
+                case 2 -> {
+                    executeSecondOption();
+                }
                 case 5 -> {
                     exit = true;
                     System.out.println("\nAturant LinkedTree...");
@@ -41,10 +44,36 @@ public class Menu {
         }
     }
 
+    private void executeSecondOption() {
+        while (true) {
+            String option = treesMenu();
+            System.out.println();
+            switch(option) {
+                case "A" -> {
+                    // Añadir algoritmo
+                }
+                case "B" -> {
+                    // Eliminar algoritmo
+                }
+                case "C" -> {
+                    // Listar algoritmos
+                }
+                case "D" -> {
+                    // Buscar por timestamp (exacto)
+                }
+                case "E" -> {
+                    // Buscar por timestamp (rango)
+                }
+                case "F" -> {
+                    return ;
+                }
+            }
+        }
+    }
+
     private void executeFirstOption() {
         while (true) {
-            followersMenu();
-            String option = askForString("Quina funcionalitat vols executar? ");
+            String option = followersMenu();
             System.out.println();
             switch (option) {
                 case "A" -> {
@@ -86,12 +115,23 @@ public class Menu {
         }
     }
 
-    private void followersMenu() {
+    private String treesMenu() {
+        System.out.println("\n\tA. Afegir algorisme");
+        System.out.println("\tB. Eliminar algorisme");
+        System.out.println("\tC. Llistar algorismes");
+        System.out.println("\tD. Cerca per timestamp (exacta)");
+        System.out.println("\tE. Cerca per timestamp (reng)");
+        System.out.println("\n\tF. Tornar entere");
+        return askForString("Quina funcionalitat vols executar? ");
+    }
+
+    private String followersMenu() {
         System.out.println("\n\tA. Explorar la xarxa.");
         System.out.println("\tB. Recomanar usuaris");
         System.out.println("\tC. Contextualizar drama");
         System.out.println("\tD. Networking");
         System.out.println("\n\tE. Tornar enrere\n");
+         return askForString("Quina funcionalitat vols executar? ");
     }
 
     private void mainMenu() {
