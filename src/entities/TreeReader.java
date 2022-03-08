742582ecf4ad44be726f5e2f6c138d1e380c2cf2
David Deu Castells <david.deu@students.salle.url.edu>
2022-03-08 09:08:55 +0100

    TreeReader added

--- src/entities/TreeReader.java
+++ src/entities/TreeReader.java
@@ -1,4 +1,39 @@
 package entities;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class TreeReader {
+
+    private final String path = "data/treeXS.paed";
+
+    public Node readFile() {
+
+        int numAlgorithms;
+
+        try {
+            File archivo = new File(path);
+            Scanner myReader = new Scanner(archivo);
+
+            numAlgorithms = Integer.parseInt(myReader.nextLine());
+
+            String input = myReader.nextLine();
+            String[] inputParse = input.split(";");
+            Node node = new Node(Integer.parseInt(inputParse[0]), inputParse[1], inputParse[2], inputParse[3], Long.parseLong(inputParse[4]));
+
+            for (int i = 0; i < numAlgorithms - 1; i++) {
+                input = myReader.nextLine();
+                inputParse = input.split(";");
+                Node nodeAux = new Node(Integer.parseInt(inputParse[0]), inputParse[1], inputParse[2], inputParse[3], Long.parseLong(inputParse[4]));
+                // insert node
+            }
+            return node;
+        } catch (FileNotFoundException e) {
+            System.out.println("The file does not exist");
+            return null;
+        }
+    }
 }
+
