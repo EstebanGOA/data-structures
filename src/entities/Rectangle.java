13e85bf050242656ea090f85d12f12be752f34f5
david.deu <david.deu@students.salle.url.edu>
2022-04-19 18:56:20 +0200

    Class Rectangle and Point added

---
+++ src/entities/Rectangle.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class Rectangle {
+    private int maxX;
+    private int maxY;
+    private int minX;
+    private int minY;
+    Rectangle[] rectangles = new Rectangle[4];
+    Point[] points = new Point[4];
+}
